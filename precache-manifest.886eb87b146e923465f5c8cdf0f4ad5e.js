06d97f1e8256dca493facb61cc98a8228340f921
Prottay Rudra <prottoyrudra7777@gmail.com>
2020-01-03 12:27:14 +0600

    Updates

---
+++ precache-manifest.886eb87b146e923465f5c8cdf0f4ad5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e331f60a22bc71ea344159852f2d44",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "0d8987be3dcc22be7e5d",
+    "url": "/dad-jokes/static/css/main.098568a4.chunk.css"
+  },
+  {
+    "revision": "13bd34d2f89b403858ef",
+    "url": "/dad-jokes/static/js/2.1c61fb1b.chunk.js"
+  },
+  {
+    "revision": "0d8987be3dcc22be7e5d",
+    "url": "/dad-jokes/static/js/main.84c13379.chunk.js"
+  },
+  {
+    "revision": "5ef66e2dbc9205e167a2",
+    "url": "/dad-jokes/static/js/runtime-main.3a5ba94b.js"
+  }
+]);
